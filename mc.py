c90d10d961baecd12d82b5c7c316567f1e6be5b4
Shengyen Li <Shenyen.li@gmail.com>
2016-02-29 17:21:31 -0500

    initiate the model calibration repo

---
+++ mc.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import os
+import time
+import numpy as np
+import ctypes
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colorbar as clb
+import matplotlib.patches as mpatches
+import matplotlib.lines as mlines
+
+
+# ++++ Fortran function for plastic deformation ++++
+import irreverisble
+
+
+def plotSingle2D(comp,xtitle,ytitle,xscale,yscale):
+
+	exp = []
+	exp = np.loadtxt('ref/850-30.dat')
+
+	fig, ax = plt.subplots(figsize=(12, 9))
+
+	ax.plot(comp[:,0],comp[:,1],lw=3)
+	ax.scatter(exp[:,0]*100, exp[:,1],s=100,zorder=5)
+
+	ax.set_xlabel(xtitle, fontsize=35, labelpad=15)
+	ax.set_ylabel(ytitle, fontsize=35, labelpad=15)
+	ax.tick_params(axis='x', labelsize=25, pad = 10)
+	ax.tick_params(axis='y', labelsize=25, pad = 10)
+
+	ax.set_xscale(xscale, nonposx='clip')
+	ax.set_yscale(yscale, nonposx='clip')
+
+	ax.grid(True)
+	fig.tight_layout()
+	plt.show()
+
+
+
+T_service = 1123 
+prec_stress = 50 
+SS_stress = 400
+
+no_samples = 1
+
+# ============================== objective
+# ============================== optimize these two parameters
+model_parameters = (-180, 3.077) 
+
+
+stress_strain, WTN = irreverisble.mechanics(prec_stress,SS_stress,T_service,model_parameters,no_samples)
+print stress_strain,len(stress_strain)
+
+stress_strain = np.array(np.trim_zeros(stress_strain)).reshape(-1,2)
+
+
+plotSingle2D(stress_strain,'strain','stress','linear','linear')
+
+
+print 'UTS, MPa: ',stress_strain[-1,0],'  Strain_UTS, %:',stress_strain[-1,1],'   Work to Necking:', WTN
+
